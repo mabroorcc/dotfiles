a8c7215a4786be4f96420e4687d22c5c031b58d5
mabroor007 <amabroor007@gmail.com>
2021-01-20 22:23:21 +0500

    backed up configuration

--- backup.sh
+++ backup.sh
@@ -3,34 +3,34 @@
 clear
 
 # vimrc
-#if [ -e /home/mabroordev/.vimrc ];then
-  #echo "vimrc found...."
-  #clear
-  #cp /home/mabroordev/.vimrc /home/mabroordev/dotfiles/.config/
-  #echo ".vimrc backup done [success]"
-#else
-  #echo "vimrc not found!"
-#fi
+if [ -e /home/mabroordev/.vimrc ];then
+  echo "vimrc found...."
+  clear
+  cp /home/mabroordev/.vimrc /home/mabroordev/dotfiles/.config/
+  echo ".vimrc backup done [success]"
+else
+  echo "vimrc not found!"
+fi
 
 #Snippets
-#if [ -d /home/mabroordev/.vim/snippets ];then
-  #echo "vim snippets found...."
-  #clear
-  #cp -r /home/mabroordev/.vim/snippets /home/mabroordev/dotfiles/.config/
-  #echo "vim snippets backup done [success]"
-#else
-  #echo "vim snippets not found!"
-#fi
+if [ -d /home/mabroordev/.vim/snippets ];then
+  echo "vim snippets found...."
+  clear
+  cp -r /home/mabroordev/.vim/snippets /home/mabroordev/dotfiles/.config/
+  echo "vim snippets backup done [success]"
+else
+  echo "vim snippets not found!"
+fi
 
 # i3 config
-#if [ -e /home/mabroordev/.config/i3/config ];then
-  #echo "i3 config found...."
-  #clear
-  #cp /home/mabroordev/.config/i3/config /home/mabroordev/dotfiles/.config/i3
-  #echo "i3 config backup done [success]"
-#else
-  #echo "i3 config not found!"
-#fi
+if [ -e /home/mabroordev/.config/i3/config ];then
+  echo "i3 config found...."
+  clear
+  cp /home/mabroordev/.config/i3/config /home/mabroordev/dotfiles/.config/i3
+  echo "i3 config backup done [success]"
+else
+  echo "i3 config not found!"
+fi
 
 git add .
 git commit -m 'backed up configuration'
